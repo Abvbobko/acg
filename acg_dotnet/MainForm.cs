7930bb11f62bdabcdc425914c707ee30ff713412
Alexei Bobko <abvbobko@gmail.com>
2020-12-03 00:26:08 +0300

    Add calculating

--- acg_dotnet/MainForm.cs
+++ acg_dotnet/MainForm.cs
@@ -40,8 +40,23 @@ namespace acg_dotnet
             Matrix<double> vertices = model.TransformCoordinates();
             List<List<int>> faces = model.FacesV;
 
-            for (int face_indx = 0; face_indx < faces.Count; face_indx++) {
+            foreach (List<int> face in faces) {
+                double x1, x2, y1, y2;
+                for (int i = 1; i < face.Count; i++) {
+                    x1 = vertices.At(0, face[i - 1] - 1);
+                    y1 = vertices.At(1, face[i - 1] - 1);
 
+                    x2 = vertices.At(0, face[i] - 1);
+                    y2 = vertices.At(1, face[i] - 1);
+
+                    // dda.dda_line(self.qp, x1, x2, y1, y2)
+                }         
+                x1 = vertices.At(0, face[0] - 1);
+                y1 = vertices.At(1, face[0] - 1);
+
+                x2 = vertices.At(0, face.Last() - 1);
+                y2 = vertices.At(1, face.Last() - 1);
+                //dda.dda_line(self.qp, x1, x2, y1, y2)
             }
 
 
