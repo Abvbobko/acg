a88da2a38bb203e3718ff6ee13a656d78333e8e1
Alexei Bobko <abvbobko@gmail.com>
2020-12-03 00:59:32 +0300

    Implement dda

--- acg_dotnet/MainForm.cs
+++ acg_dotnet/MainForm.cs
@@ -36,42 +36,62 @@ namespace acg_dotnet
 
             // Defines pen 
             Pen pen = new Pen(ForeColor);
+            Brush brush = Brushes.Black;
 
             Matrix<double> vertices = model.TransformCoordinates();
             List<List<int>> faces = model.FacesV;
 
             foreach (List<int> face in faces) {
                 double x1, x2, y1, y2;
-                for (int i = 1; i < face.Count; i++) {
+                for (int i = 1; i < face.Count; i++) {                    
                     x1 = vertices.At(0, face[i - 1] - 1);
                     y1 = vertices.At(1, face[i - 1] - 1);
 
                     x2 = vertices.At(0, face[i] - 1);
                     y2 = vertices.At(1, face[i] - 1);
-
-                    // dda.dda_line(self.qp, x1, x2, y1, y2)
+                    pea.Graphics.DrawLine(pen, new Point(Convert.ToInt32(x1), Convert.ToInt32(y1)), new Point(Convert.ToInt32(x2), Convert.ToInt32(y2)));
+                    //DDA_Line(pea, brush, x1, x2, y1, y2);
                 }         
                 x1 = vertices.At(0, face[0] - 1);
                 y1 = vertices.At(1, face[0] - 1);
 
                 x2 = vertices.At(0, face.Last() - 1);
                 y2 = vertices.At(1, face.Last() - 1);
-                //dda.dda_line(self.qp, x1, x2, y1, y2)
+
+                pea.Graphics.DrawLine(pen, new Point(Convert.ToInt32(x1), Convert.ToInt32(y1)), new Point(Convert.ToInt32(x2), Convert.ToInt32(y2)));
+                //DDA_Line(pea, brush, x1, x2, y1, y2);                
+            }
+        
+        }
+
+        private void DDA_Line(PaintEventArgs pea, Brush brush, double x1, double x2, double y1, double y2) {            
+            double L = Math.Max(Math.Abs(x1 - x2), Math.Abs(y1 - y2));
+            double dx = (x2 - x1) / L;
+            double dy = (y2 - y1) / L;
+
+            List<double> x = new List<double>();
+            List<double> y = new List<double>();
+            int i = 0;
+            x.Add(x1);
+            y.Add(y1);
+            i += 1;
+
+            while (i < L) {
+                x.Add(x[i - 1] + dx);
+                y.Add(y[i - 1] + dy);
+                i += 1;
             }
 
-
-
-           
-
-            // Defines the both points to connect 
-            // pt1 is (30.0, 30.0) which represents (x1, y1) 
-            PointF pt1 = new PointF(30.0F, 30.0F);
-
-            // pt2 is (200.0, 300.0) which represents (x2, y2) 
-            PointF pt2 = new PointF(200.0F, 300.0F);
-
-            // Draws the line 
-            pea.Graphics.DrawLine(pen, pt1, pt2);
+            x.Add(x2);
+            y.Add(y2);
+            for (int j = 0; j < x.Count; j++) {
+                Console.WriteLine(x[j]);
+            }
+            while (i <= L) {
+                //Console.WriteLine(x[i].ToString() + " " + y[i].ToString());
+                pea.Graphics.FillRectangle(brush, Convert.ToSingle(x[i]), Convert.ToSingle(y[i]), 1, 1);
+                i += 1;
+            }            
         }
 
     }
