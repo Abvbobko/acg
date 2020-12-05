0d1c36f0a1a75aa788bc05fac789f2141d9300b6
Alexei Bobko <abvbobko@gmail.com>
2020-12-06 00:37:46 +0300

    Add zbuffer class

--- acg_dotnet/MainForm.cs
+++ acg_dotnet/MainForm.cs
@@ -27,12 +27,13 @@ namespace acg_dotnet
 
     public partial class MainForm : Form {
         Model model;
+        ZBuffer zBuffer;
 
         public MainForm() {
             InitializeComponent();
 
             //Size = new Size(Constants.WIN_WIDTH, Constants.WIN_HEIGHT);
-            model = new Model();
+            model = new Model();            
         }
 
         protected override void OnPaint(PaintEventArgs pea) {
@@ -48,30 +49,35 @@ namespace acg_dotnet
             Matrix<double> vertices = model.TransformCoordinates();
             List<List<int>> faces = RejectFaces(model.FacesV, vertices);
 
+            zBuffer = new ZBuffer(Size.Width, Size.Height);
+
             foreach (List<int> face in faces) {
                 List<double[]> polygonPoints = new List<double[]>();
-                double x1, x2, y1, y2;
+                double x1, x2, y1, y2, z1, z2;
                 for (int i = 1; i < face.Count; i++) {
                     x1 = vertices.At(0, face[i - 1] - 1);
                     y1 = vertices.At(1, face[i - 1] - 1);
+                    z1 = vertices.At(2, face[i - 1] - 1);
 
                     x2 = vertices.At(0, face[i] - 1);
                     y2 = vertices.At(1, face[i] - 1);
+                    z2 = vertices.At(2, face[i] - 1);
                     //pea.Graphics.DrawLine(pen, new Point(Convert.ToInt32(x1), Convert.ToInt32(y1)), new Point(Convert.ToInt32(x2), Convert.ToInt32(y2)));
-                    polygonPoints.AddRange(DDA_Line(x1, x2, y1, y2));
+                    polygonPoints.AddRange(DDA_Line(x1, x2, y1, y2, z1, z2));
                 }
                 x1 = vertices.At(0, face[0] - 1);
                 y1 = vertices.At(1, face[0] - 1);
+                z1 = vertices.At(2, face[0] - 1);
 
                 x2 = vertices.At(0, face.Last() - 1);
                 y2 = vertices.At(1, face.Last() - 1);
-
+                z2 = vertices.At(2, face.Last() - 1);
                 Brush brush1 = Brushes.Green;
 
                 double x1_ = vertices.At(0, face[0] - 1), y1_ = vertices.At(1, face[0] - 1);
                 double x2_ = vertices.At(0, face[1] - 1), y2_ = vertices.At(1, face[1] - 1);
                 double x3_ = vertices.At(0, face[2] - 1), y3_ = vertices.At(1, face[2] - 1);
-                NewFillPolygon(
+                FillPolygon(
                     pea, 
                     brush1,
                     Convert.ToInt32(Math.Round(x1_)), Convert.ToInt32(Math.Round(y1_)),
@@ -80,7 +86,7 @@ namespace acg_dotnet
                 );
 
                 //pea.Graphics.DrawLine(pen, new Point(Convert.ToInt32(x1), Convert.ToInt32(y1)), new Point(Convert.ToInt32(x2), Convert.ToInt32(y2)));
-                polygonPoints.AddRange(DDA_Line(x1, x2, y1, y2));
+                polygonPoints.AddRange(DDA_Line(x1, x2, y1, y2, z1, z2));
 
                 //FillPolygon(pea, polygonPoints);
                 DrawPoints(pea, brush, polygonPoints);
@@ -93,7 +99,7 @@ namespace acg_dotnet
             return new int[] { b, a };
         }
 
-        private void NewFillPolygon(PaintEventArgs pea, Brush brush, int x0, int y0, int x1, int y1, int x2, int y2) {
+        private void FillPolygon(PaintEventArgs pea, Brush brush, int x0, int y0, int x1, int y1, int x2, int y2) {
             if (y0 > y1) {
                 int[] tmp = Swap(y0, y1);
                 y0 = tmp[0];
@@ -183,73 +189,25 @@ namespace acg_dotnet
             }
 
             return visible_faces;
-        }   
+        }          
 
-        private void FillPolygon(PaintEventArgs pea, List<double[]> polygonPoints) {           
-            polygonPoints = polygonPoints.OrderBy(xy => xy[1]).ToList(); // sort by y
-                                                                         //polygonPoints = polygonPoints.OrderBy(xy => xy[0]).ToList(); // sort by x
-               
-            Brush brush = Brushes.Green;
-            int i = 0;
-            int first_y = 0;
-            while (first_y < polygonPoints.Count) {                
-                // add all x to x list
-                List<double> x = new List<double>();
-
-                int y_tmp = Convert.ToInt32(polygonPoints[first_y][1]);
-                //x.Add(y_tmp);
-                first_y += 1;
-                for (int j = first_y; j < polygonPoints.Count; j++) {
-                    if (Convert.ToInt32(polygonPoints[j][1]) != y_tmp) {
-                        first_y = j;
-                        break;
-                    }
-                    x.Add(polygonPoints[j][0]);
-                }
-                x.Sort();                       
-
-                if (x.Count > 1) {
-                    for (int k = 0; k < x.Count-1; k += 2) {                        
-                        double x1 = x[k];
-                        double x2 = x[k+1];
-                        //Console.Write(x1.ToString() + " " + x2.ToString() + " | ");                        
-
-                        DrawPoints(pea, brush, DDA_Line(x1, x2, y_tmp, y_tmp));
-                    }
-                }
-                //Console.Write("\n");
-            }
-        } 
-
-        private List<double[]> DDA_Line(double x1, double x2, double y1, double y2) {            
+        private List<double[]> DDA_Line(double x1, double x2, double y1, double y2, double z1, double z2) {            
             double L = Math.Max(Math.Abs(x1 - x2), Math.Abs(y1 - y2));
             double dx = (x2 - x1) / L;
             double dy = (y2 - y1) / L;
 
-            List<double[]> xy = new List<double[]>();
-            //List<double> y = new List<double>();
+            List<double[]> xy = new List<double[]>();            
             int i = 0;
-            xy.Add(new double[] { x1, y1 });
-            //x.Add(x1);
-            //y.Add(y1);
+            xy.Add(new double[] { x1, y1 });           
             i += 1;
 
             while (i < L) {
-                xy.Add(new double[] { xy[i - 1][0] + dx, xy[i - 1][1] + dy });
-                //x.Add(x[i - 1] + dx);
-                //y.Add(y[i - 1] + dy);
+                xy.Add(new double[] { xy[i - 1][0] + dx, xy[i - 1][1] + dy });                
                 i += 1;
             }
 
             xy.Add(new double[] { x2, y2 });
-            //x.Add(x2);
-            //y.Add(y2);
-            /* i = 0;
-            while (i <= L) {                                
-                //Console.WriteLine(x[i].ToString() + " " + y[i].ToString());
-                pea.Graphics.FillRectangle(brush, Convert.ToSingle(xy[i][0]), Convert.ToSingle(xy[i][1]), 1, 1);
-                i += 1;
-            } */
+
             return xy;//new Tuple<List<double>, List<double>>(x, y);
         }
 
---
+++ acg_dotnet/Tools/ZBuffer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acg_dotnet.Tools
+{
+    class ZBuffer
+    {
+        private double[,] buffer;
+        private int width;
+        private int height;
+
+        public ZBuffer(int width, int height) {
+            this.width = width;
+            this.height = height;
+
+            buffer = new double[width, height];
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height; j++) {
+                    buffer[i, j] = double.PositiveInfinity;
+                }
+            }
+        }
+
+        public double this[int x, int y] {
+            get {
+                if ((x < width) && (y < height)) {
+                    return buffer[x, y];
+                }                
+                return float.NegativeInfinity;
+            }
+
+            set {
+                if ((x < width) && (y < height)) {
+                    buffer[x, y] = value;
+                }                
+            }
+        }
+
+
+    }
+}
